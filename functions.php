44b9deb7f8d272e290f70a9b3cc68e96d52fdd36
tcicit <tcicit@gmail.com>
2020-10-28 09:29:17 +0100

    Update functions.php
    Disable rest API interface for unregistered users

--- functions.php
+++ functions.php
@@ -7,6 +7,20 @@ function register_my_scripts() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css'  );
 }
 
+
+// Disable rest API interface for unregistered users
+add_filter( 'rest_authentication_errors', function( $result ) { 
+  if ( ! empty( $result ) ) { 
+    return $result; 
+  } 
+  if ( ! is_user_logged_in() ) { 
+    return new WP_Error( '401', 'not allowed.', array('status' => 401) ); 
+  } 
+  return $result;
+});
+
+
+
 ?>
 
 
